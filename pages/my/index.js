3e6466df91f9ff81a3b22164a828c3b8ef3e7799
junhaozhang <119581712+202264642365@users.noreply.github.com>
2023-08-23 15:38:50 +0800

    Update /my/index.js

--- pages/my/index.js
+++ pages/my/index.js
@@ -3,9 +3,28 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    //个人信息栏  
+    username:"蔡宣轩",
+    userwork:"设计/艺术从业者",
+    userage:"29",
+    image: 'https://tdesign.gtimg.com/miniprogram/images/avatar1.png',
+    //tab选项卡
+    tabPanelstyle: 'display:flex;justify-content:center;align-items:center;',
   },
-  onLoad() {
+  // 图标tap事件
+  onIconTap(){},
+  /**
+   * tab选项卡
+   */ 
+  onTabsChange(event) {
+    console.log(`Change tab, tab-panel value is ${event.detail.value}.`);
+  },
 
-  }
+  onTabsClick(event) {
+    console.log(`Click tab, tab-panel value is ${event.detail.value}.`);
+  }, 
+  
+  onStickyScroll(event) {
+    console.log(event.detail);
+  },  
 })
