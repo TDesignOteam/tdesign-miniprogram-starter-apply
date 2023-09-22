d0e17e79b0056b55d37f514a9af8f77a1fa96142
Lindesay Bravill <132528362+LindsBravill@users.noreply.github.com>
2023-09-22 16:13:13 +0800

    Revert "feat/my"

--- pages/my/index.js
+++ pages/my/index.js
@@ -1,29 +1,11 @@
-// index.js
-const app = getApp();
-import {
-  fetchMyatvs
-} from '../../services/fetchMyAtvs';
+// logs.js
+const util = require('../../utils/util.js')
 
 Page({
   data: {
-    navBarHeight: app.globalData.navBarHeight,
-    profile: "https://tdesign.gtimg.com/miniprogram/images/avatar1.png",
-    username: '蔡宣轩',
-    tags: ['29岁', '设计/艺术从业者'],
-    myAtvs: []
+    logs: []
   },
-
-  // 加载页面数据
-  async loadData() {
-    const {
-      data
-    } = await fetchMyatvs();
-    this.setData({
-      myAtvs: data
-    })
-  },
-
   onLoad() {
-    this.loadData();
+
   }
 })
